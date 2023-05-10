9c2688dabbcb768abb9f7d1cc99abe47e9a2529d
Newtayo <akandeabdul@gmail.com>
2023-05-10 14:40:43 +0100

    Add method to decord morse message

--- morse_code.rb
+++ morse_code.rb
@@ -36,9 +36,9 @@ class MorseCodeDecoder
   def self.decode_word(morse_word)
     morse_word.split(" ").map { |morse_char| decode_char(morse_char) }.join("")
   end
-  
+
   def self.decode(message)
-    message.split("   ").map { |morse_word| decode_word(morse_word) }.join("")
+    message.split("   ").map { |morse_word| decode_word(morse_word) }.join(" ")
   end
 end
 # Usage example
