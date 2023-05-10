48768d4f572f27a4faaf622024818bfd5c912406
Newtayo <akandeabdul@gmail.com>
2023-05-10 14:38:19 +0100

    Add method to decord morse message

--- morse_code.rb
+++ morse_code.rb
@@ -27,3 +27,20 @@ class MorseCodeDecoder
     "-.--" => "Y",
     "--.." => "Z"
   }
+
+
+ def self.decode_char(morse_char)
+    MORSE_CODE[morse_char]
+  end
+
+  def self.decode_word(morse_word)
+    morse_word.split(" ").map { |morse_char| decode_char(morse_char) }.join("")
+  end
+  
+  def self.decode(message)
+    message.split("   ").map { |morse_word| decode_word(morse_word) }.join("")
+  end
+end
+# Usage example
+puts MorseCodeDecoder.decode(".-   -... --- -..- ..-. ..- .-.. .-..     --- ..-.   .-. ..- -... .. . ...")
+# Output: "ABXFULL OF BEES"
